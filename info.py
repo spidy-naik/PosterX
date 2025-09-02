810c5cac4fb9d02fac95ab26302a263580d9b29c
Spidy Naik <166608100+spidy-naik@users.noreply.github.com>
2025-09-02 16:35:04 +0530

    xx

--- info.py
+++ info.py
@@ -6,7 +6,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = int(environ.get('API_ID', '20389440'))
 API_HASH = environ.get('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457')
-BOT_TOKEN = environ.get('BOT_TOKEN', '8114630449:AAEO-PJCv__XMb4HrnJAFWfuq_zHJ9GTWj0')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7405088923:AAG3oNxF-pgHq4g_dCX6zdNlRwsek_VSoEk')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1855665805 5602400935').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "") 
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
