d49c6aa8fffd2e0f7070cc6da5a3f996dbdd5fbf
Spidy Naik <166608100+spidy-naik@users.noreply.github.com>
2025-09-03 00:02:26 +0530

    Done

--- info.py
+++ info.py
@@ -4,11 +4,11 @@ from os import environ, getenv
 
 id_pattern = re.compile(r'^.\d+$')
 
-API_ID = int(environ.get('API_ID', '20389440'))
-API_HASH = environ.get('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7405088923:AAG3oNxF-pgHq4g_dCX6zdNlRwsek_VSoEk')
+API_ID = int(environ.get('API_ID', '25412293'))
+API_HASH = environ.get('API_HASH', 'd58456be9931f3f6b8154a626fc1b3c6')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7405088923:AAFfF3pDlpvp2Ou5duXROhzOkRIsTxu0oEA')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5602400935').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "") 
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'PosterBoot')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'PosterBot')
 PORT = environ.get("PORT", "8080")
